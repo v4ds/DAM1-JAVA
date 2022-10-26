982a811f07febbedc8d8e78bda204965c250fa8d
Vadim <116269814+v4ds@users.noreply.github.com>
2022-10-27 00:30:04 +0200

    Add files via upload

--- Unit2/E1X04.java
+++ Unit2/E1X04.java
@@ -1,14 +1,13 @@
-package P1;
 
 public class E1X04 {
 	
 	public static void main(String[] args) {
-		for (int a = 2; a <= 6; a++) {
-			for (int b= 1; b < 10; b++) {
-				System.out.printf("%d ^ %d = %d", b, a, elevar(b, a));
+		for (int a = 1; a <= 9; a++) {
+			for (int b= 2; b <= 6; b++) {
+				System.out.printf(" %d ^ %d = %6d", a, b, elevar(a, b));
 				System.out.print(", ");
 			}
-			System.out.print("\n");
+			System.out.println();
 		}
 	}
 
